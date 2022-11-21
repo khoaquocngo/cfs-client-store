686f00e3b8ec8563cbee30fc1e2c1c458d2a591d
Tai Pham <tai.ph@urbox.vn>
2022-11-21 21:37:12 +0700

    init tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  module: 'jit',
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('flowbite/plugin')],
+};
