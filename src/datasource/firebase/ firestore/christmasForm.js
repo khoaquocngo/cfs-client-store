833e8e8fad3699291ca8e60e570507a0fc0bd6e5
Khoa Ngo <nmkhoa17ck1@gmail.com>
2022-12-16 00:02:34 +0700

    update: Format uuid

--- src/datasource/firebase/ firestore/christmasForm.js
+++ src/datasource/firebase/ firestore/christmasForm.js
@@ -2,16 +2,30 @@ import { addDoc } from 'firebase/firestore';
 
 import { ChristmasForm } from './emtity';
 
+function formatNameLength(fisrtName = '', lastName = '') {
+  if (lastName.length > 7) {
+    return lastName.slice(0, 7);
+  }
+
+  if (fisrtName.length > 7) {
+    return lastName;
+  }
+
+  return !fisrtName ? `${lastName}` : `${fisrtName}_${lastName}`;
+}
+
+function getFisrtUuid(name) {
+  if (!name.replaceAll(' ', '')) return '';
+  const listName = name.trim().split(' ');
+
+  return formatNameLength(listName.at(-2), listName.at(-1));
+}
+
 function getUuid(name, date) {
-  const firstUUid = name
-    .normalize('NFD')
-    .replace(/[\u0300-\u036f]/g, '')
-    .replace(/đ/g, 'd')
-    .replace(/Đ/g, 'D')
-    .replaceAll(' ', '_');
+  const firstUUid = getFisrtUuid(name);
   const lastUuid = `${+date}`.slice(-5);
 
-  return `${firstUUid}_${lastUuid}`;
+  return firstUUid ? `${firstUUid}_${lastUuid}` : lastUuid;
 }
 
 export const createChristmasForm = async function (input = {}) {
@@ -31,11 +45,9 @@ export const createChristmasForm = async function (input = {}) {
       refcode: '',
     };
 
-    const a = await (await addDoc(ChristmasForm, createData)).id;
-    console.log("===createData===", createData);
-    console.log('aaaa', a);
+    const christmasFormId = await (await addDoc(ChristmasForm, createData)).id;
 
-    if (!a) {
+    if (!christmasFormId) {
       throw Error('Create Christmas Form failure');
     }
 
